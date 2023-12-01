8d6bed9056ff5911bde6304fade7ef6596b1e438
fuadmoin <fuadamoin@gmail.com>
2023-12-01 05:01:17 +0300

    Answer python import modules.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print("a + b = {}".format(add(a, b)))
+print("a - b = {}".format(sub(a, b)))
+print("a * b = {}".format(mul(a, b)))
+print("a / b = {}".format(div(a, b)))
